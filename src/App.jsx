cfe85b95da560aba907657bfd6da194c31aa9c6d
lvlashnin <lvlashnin@users.noreply.github.com>
2018-07-10 22:10:04 +0300

    bind(null,props)

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,18 @@ import ProReactForms from './ProReactForms'
 import ReactComponents from './ReactComponents';
 
 class App extends Component {  
+  constructor() {
+		super()
+
+		this.state = {
+      user1: {name: 'Коля', surname: 'Иванов'},
+			user2: {name: 'Вася', surname: 'Петров'},
+			user3: {name: 'Петя', surname: 'Сидоров'},
+		}
+  }
+  showMessage(num) {
+    alert(num);
+  }
   render() {    
     return (
       <div>
@@ -18,7 +30,26 @@ class App extends Component {
       <ProCycles/>
       {/* <ReactForms/> */}
       <ProReactForms/>
-      <ReactComponents/>     
+      <ReactComponents
+				name={this.state.user1.name}
+				surname={this.state.user1.surname}
+				num="1"
+				showMessage={this.showMessage.bind(this)}
+			/>
+			
+			<ReactComponents
+				name={this.state.user2.name}
+				surname={this.state.user2.surname}
+				num="2"
+				showMessage={this.showMessage.bind(this)}
+			/>
+			
+			<ReactComponents
+				name={this.state.user3.name}
+				surname={this.state.user3.surname}
+				num="3"
+				showMessage={this.showMessage.bind(this)}
+			/>    
       </div>
     );
   }
--- src/ReactComponents.jsx
+++ src/ReactComponents.jsx
@@ -2,25 +2,17 @@ import React, { Component } from 'react';
 
 class ReactComponents extends Component {
     constructor() {
-		super();
+        super()
+    }      
 
-		//Храним данные в стейте:
-    this.state = {
-			users: [
-				{name: 'Коля', surname: 'Иванов', age:'23'},
-				{name: 'Вася', surname: 'Петров', age:'24'},
-				{name: 'Петя', surname: 'Сидоров', age:'27'},
-			]
-		}
-	}
     
-    render() {
-        const list = this.state.users.map((user, index)=>{
-            return <p key = {index}>NAME:{user.name},SURNAME:{user.surname},AGE:{user.age}</p>
-        })
-        return (
-            <div>
-            {list}
+    render() {        
+        return (<div>
+           	<p>имя: {this.props.name}</p>
+			<p>фамилия: {this.props.surname}</p>
+            <button onClick={this.props.showMessage.bind(null, this.props.num)}>
+				нажми на меня
+			</button>
             </div>
         )
     }
